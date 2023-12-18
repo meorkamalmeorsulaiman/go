56323c24546f05dd1ead9418e9d29cef77534d51
Meor Muhammad Kamal Bin Meor Muhammad Sulaiman <kamal.sulaiman@paynet.my>
2023-12-18 12:27:54 +0800

    parsing json

--- api/src/api.go
+++ api/src/api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,6 +9,23 @@ import (
 	"os"
 	"crypto/tls"
 )
+
+// A Response struct to map the Entire Response
+type Response struct {
+    Name    string    `json:"name"`
+    Pokemon []Pokemon `json:"pokemon_entries"`
+}
+
+// A Pokemon Struct to map every pokemon to.
+type Pokemon struct {
+    EntryNo int            `json:"entry_number"`
+    Species PokemonSpecies `json:"pokemon_species"`
+}
+
+// A struct to map our Pokemon's Species which includes it's name
+type PokemonSpecies struct {
+    Name string `json:"name"`
+}
 
 func main() {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
@@ -22,6 +40,17 @@ func main() {
     if err != nil {
         log.Fatal(err)
     }
-    fmt.Println(string(responseData))
+
+    //fmt.Println(string(responseData))
+
+    var responseObject Response
+    json.Unmarshal(responseData, &responseObject)
+
+    fmt.Println(responseObject.Name)
+    fmt.Println(len(responseObject.Pokemon))
+
+    for  i := 0; i < len(responseObject.Pokemon); i++ {
+        fmt.Println(responseObject.Pokemon[i].Species.Name)
+    }
 
 }
